418ef1db2ca151d61557005b461f0e4f56c12a43
wiladhianty <wiladhianty@gmail.com>
2021-06-30 11:02:13 +0700

    tambah tugas

---
+++ tugas7.js
@@ -0,0 +1,11 @@
+const tinggi = [160, 170, 155, 160, 166, 180, 176, 175, 169, 173]
+let i = 0
+
+for(i;i<tinggi.length;i++){
+    console.log(tinggi[i])
+}
+
+// memakai For Of
+for(x of tinggi){
+    console.log(x)
+}
